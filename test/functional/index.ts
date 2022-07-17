989212e4255b3c9787846895453dfe8a8d499783
Owen Wu <yubing744@163.com>
2022-07-17 15:47:36 +0800

    feat: seq run functional test

--- test/functional/index.ts
+++ test/functional/index.ts
@@ -14,15 +14,14 @@ export function run(): Promise<void> {
   const mocha = new Mocha({
     ui: 'tdd',
     color: true,
-    grep: process.env.MOCHA_grep,
     timeout: 1000 * 60 * 20,
-    parallel: false,
+    parallel: false
   });
 
   const testsRoot = path.resolve(__dirname, '.');
 
   return new Promise((c, e) => {
-    glob('**/*.test.js', { cwd: testsRoot }, (err, files) => {
+    glob(process.env.MOCHA_grep || '', { cwd: testsRoot }, (err, files) => {
       if (err) {
         return e(err);
       }
