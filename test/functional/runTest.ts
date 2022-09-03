f7e027b8d1a7af7abe1815b3795ee49e2e26ccd5
Owen Wu <yubing744@163.com>
2022-09-03 15:24:13 +0800

    feat: wait to tests ready

--- test/functional/runTest.ts
+++ test/functional/runTest.ts
@@ -21,7 +21,7 @@ async function main(): Promise<void> {
 
   // The workspace
   const testWorkspace = path.resolve(__dirname, './demos/simple-nft-mpm');
-  let testWorkspacePath = path.resolve(testWorkspace, './simple-nft.code-workspace');
+  const testWorkspacePath = path.resolve(testWorkspace, './simple-nft.code-workspace');
 
   // Install vscode and depends extension
   const vscodeVersion = '1.64.0';
